9ccb262a5865f3434e97ae9b6adeeeaa2f6df5e5
Holman Alba <holman.alba@repremundo.com.co>
2025-10-25 23:48:37 -0500

    fix(tour): lazy-load driver.js (JS+CSS) and add dependency to avoid Vite build error in CI

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import './assets/tailwind.css'
 import App from './App.vue'
 import { MotionPlugin } from '@vueuse/motion'
-import 'driver.js/dist/driver.css'
 
 
 createApp(App).use(MotionPlugin).mount('#app')
